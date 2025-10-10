6b0b00f23385c75edabdfed68df469716f5b4a96
mikek182 <marcosvinicius@geekwork.com.br>
2025-10-10 13:36:03 -0300

    Imprementação de icones na section serviços

--- src/app/components/cards/index.tsx
+++ src/app/components/cards/index.tsx
@@ -1,12 +1,7 @@
 import styles from './styles.module.scss'
-import { FileInputIcon } from 'lucide-react';
 
-export default function Card() {
+export default function Card({ children }: { children?: React.ReactNode }) {
     return (
-        <div className={styles.card}>
-            <FileInputIcon size={48} />
-            <h2>Card Title</h2>
-            <p>Card content goes here.</p>
-        </div>
+        <div className={styles.card}>{children}</div>
     )
 }
